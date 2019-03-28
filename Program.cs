529025a07eb86aa0390e7cb8ec726fdfd16e77ad
Gerardo Lijs <gerardo.lijs@gmail.com>
2019-03-28 14:46:30 +0100

    netcore sample app

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TelemetrySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Remember to configure your own InstrumentationKey before starting in Telemetry.cs");
+
+            Telemetry.TrackPageView("ProgramStarted");
+
+            Console.WriteLine("Testing a div by zero exception");
+            try
+            {
+                int div = 0;
+                int i = 1 / div;
+            }
+            catch (Exception ex)
+            {
+                Telemetry.TrackException(ex);
+            }
+
+            // Flush telemetry before program exit. It takes a couple of minutes for temetry to show in Azure
+            // Don't do it after every call to Telemetry, your metrics will be sent in batches for performance.
+            Telemetry.Flush();
+            Console.WriteLine("Telemetry sent");
+
+            // Close program
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Telemetry.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.ApplicationInsights;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelemetrySample
+{
+    public static class Telemetry
+    {
+        // Consider using DI. This is just a quick sample
+        private static TelemetryClient tc;
+
+        static Telemetry()
+        {
+            tc = new TelemetryClient
+            {
+                // TODO: replace with your own InstrumentationKey
+                InstrumentationKey = "f304267a-0d47-4378-8333-secretkey"
+            };
+
+            // Set global properties if needed like unique installation number or customer id
+            //tc.Context.GlobalProperties.Add("InstallationId", "unique_id_for_your_installation");
+
+            // Set new SessionId
+            tc.Context.Session.Id = Guid.NewGuid().ToString();
+
+            // Set version if needed
+            //tc.Context.Component.Version = Application.ProductVersion;
+
+            // Set other data if needed
+            tc.Context.User.Id = Environment.UserName;
+            tc.Context.Device.OperatingSystem = Environment.OSVersion.ToString();
+        }
+
+        public static void Flush() => tc.Flush();
+        public static void TrackException(Exception ex) => tc.TrackException(ex);
+        public static void TrackPageView(string name) => tc.TrackPageView(name);
+        public static void TrackEvent(string eventName, IDictionary<string, string> properties = null, IDictionary<string, double> metrics = null) => tc.TrackEvent(eventName, properties, metrics);
+    }
+}
